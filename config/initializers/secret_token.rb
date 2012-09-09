380c6c360a5e979dd8373ed7cdc68c3c6b24f96c
Sean Carey <naryasece@gmail.com>
2012-09-09 13:52:31 -0700

    initial commit of instapopular
    pretty much just the empty applicaiton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instapopular::Application.config.secret_token = '81c8bbc7ad71fa7b4fb80d0831f7904663de36c42dbd965d8dd4650a37a200bdd8f3d607172efca05e8f002ebf1434972faec336ceb07bfcd00fca20afe6468f'
